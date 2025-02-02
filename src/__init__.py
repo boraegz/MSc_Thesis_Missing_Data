fc17d6cd659cd7ef68798af57cc68c48481e2ad9
boraegz <boraegz@gmail.com>
2025-02-02 22:59:20 +0100

    test 3

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,6 @@
 # Import classes and functions from other modules so they can be accessed directly
 from .data_simulator import DataSimulator
 from .missing_data_handler import MissingDataHandler
-from .models import ModelTrainer
+from .model import Model
 from .evaluation import evaluate_model
 from .utils import split_data, visualize_results
